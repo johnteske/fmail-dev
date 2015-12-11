a8089daf6b53accf3d2aa1415ee6a7187789aef9
John Teske <john.teske@gmail.com>
2015-12-10 16:44:10 -0800

    Remove console.log from checkProj()

--- Gruntfile.js
+++ Gruntfile.js
@@ -5,7 +5,7 @@ module.exports = function(grunt) {
     // project
     function checkProj() {
         var proj = grunt.option('project');
-        if (proj) { console.log( grunt.option('project') );
+        if (proj) {
             if (!grunt.file.exists('src/' + proj)) {
                 grunt.fail.fatal('Project folder does not exist.\nTry running commands from the src/ directory to use auto-complete on folder names.')
             }
